d0dae51e453244c866cd9a3cd0c4e60d4000c83b
srabich-makor <86242978+srabich-makor@users.noreply.github.com>
2022-05-10 09:33:36 +0300

    adding the first test case

---
+++ cypress/integration/auth.spce.js
@@ -0,0 +1,7 @@
+describe("Drag n Drop", function () {
+  it("allows moving piece back to open spot", function () {
+    cy.visit("https://dev.crypto-company.enigma-x.app");
+
+    cy.get('[name="email"]').should("exist");
+  });
+});
